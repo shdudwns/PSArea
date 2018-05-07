f4f1bb5c3c14aa1e67a9bd3d0735b413b75a1a6e
Potatoshare88 <potatoshare88@gmail.com>
2018-05-07 10:45:43 +0900

    Can get From Offline player
    로그야, 타입은 명확하게 하자

--- src/ps88/psarea/MoneyTranslate/MoneyTranslator.php
+++ src/ps88/psarea/MoneyTranslate/MoneyTranslator.php
@@ -22,6 +22,7 @@
 
         public function getMoney($player): ?int {
             $pl = ($player instanceof Player) ? $player : Server::getInstance()->getPlayer($player);
+            if($pl == \null and $pl = Server::getInstance()->getOfflinePlayer($player) == \null) return \null;
             if ($this->moneyAPI instanceof EconomyAPI) {
                 return $this->moneyAPI->myMoney($player);
             } elseif ($this->moneyAPI instanceof StormCore && $pl instanceof StormPlayer) {
@@ -30,8 +31,9 @@
             return \null;
         }
 
-        public function addMoney($player, int $money): void {
+        public function addMoney($player, int $money): bool {
             $pl = ($player instanceof Player) ? $player : Server::getInstance()->getPlayer($player);
+            if($pl == \null and $pl = Server::getInstance()->getOfflinePlayer($player) == \null) return \false;
             if ($this->moneyAPI instanceof EconomyAPI) {
                 $this->moneyAPI->addMoney($player, $money);
                 return true;
@@ -42,8 +44,9 @@
             return false;
         }
 
-        public function reduceMoney($player, int $money): void {
+        public function reduceMoney($player, int $money): bool {
             $pl = ($player instanceof Player) ? $player : Server::getInstance()->getPlayer($player);
+            if($pl == \null and $pl = Server::getInstance()->getOfflinePlayer($player) == \null) return \false;
             if ($this->moneyAPI instanceof EconomyAPI) {
                 $this->moneyAPI->reduceMoney($player, $money);
                 return true;
@@ -54,8 +57,9 @@
             return false;
         }
 
-        public function setMoney($player, int $money): void {
+        public function setMoney($player, int $money): bool {
             $pl = ($player instanceof Player) ? $player : Server::getInstance()->getPlayer($player);
+            if($pl == \null and $pl = Server::getInstance()->getOfflinePlayer($player) == \null) return \false;
             if ($this->moneyAPI instanceof EconomyAPI) {
                 $this->moneyAPI->setMoney($player, $money);
                 return true;
